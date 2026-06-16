f955d913ae94dc099781aaabc0c21c536553d728
quintongillanders <142285437+quintongillanders@users.noreply.github.com>
2026-06-16 12:07:05 +1200

    removed fh1Cars.jsx, and added fh1cars.json

--- src/pages/FH1.jsx
+++ src/pages/FH1.jsx
@@ -1,15 +1,32 @@
-import { fh1Cars } from "../data/fh1Cars";
+import fh1Cars from "../data/fh1cars.json";
 import "./FH1.css";
 
 export default function FH1() {
   return (
     <div className="grid">
-      {fh1Cars.map((car) => (
-        <div className="card" key={car.id}>
-          <h3>{car.name}</h3>
-          <p>{car.year} • {car.manufacturer}</p>
-          <p>{car.bodyStyle}</p>
-          <span>{car.pi}</span>
+      {fh1Cars.map((car, index) => (
+        <div className="card" key={index}>
+          <h3>
+            {car.Make} {car.Model}
+          </h3>
+
+          <p>
+            {car.Year} • {car.Country}
+          </p>
+
+          <p>{car.Body_Style}</p>
+
+          <p>
+            {car.Class} {car.Stock_PI}
+          </p>
+
+          <p>
+            {car.Horsepower} HP • {car.Drivetrain}
+          </p>
+
+          <p>
+            {Number(car.Base_Value_Credits).toLocaleString()} CR
+          </p>
         </div>
       ))}
     </div>
